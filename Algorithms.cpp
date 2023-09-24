ef12025824d08bc87545cb4503348d2da00c0a6e
fox5352 <voschristopher53@gmail.com>
2023-09-24 09:42:26 -0700

    created print function

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -5,24 +5,25 @@
 
 #include "Sort_Algorithms.h"
 
+void print(int arry[],int arry_size){
+    for (size_t i = 0; i < arry_size; i++)
+    {
+        std::cout << arry[i] << " ";
+    }
+    std::cout << " " << std::endl;
+}
 
 int main()
 {
     int arry[] = {8,4,5,6,0,2,1};
     int arry_size = sizeof(arry) / sizeof(int);
 
-    for (size_t i = 0; i < arry_size; i++)
-    {
-        std::cout << arry[i] << " ";
-    }
-    std::cout << " " << std::endl;
 
+
+    print(arry, arry_size);
     Sort_Algorithms::bubble_sort(std::move(arry), arry_size);
+    print(arry, arry_size);
 
-    for (size_t i = 0; i < arry_size; i++)
-    {
-        std::cout << arry[i] << " ";
-    }
     std::cout << " " << std::endl;
 
     std::cout << "Hello World!\n";
